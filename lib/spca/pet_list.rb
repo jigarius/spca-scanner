1444a78a8d88fbf1bb9c4a8864c56be48b911869
Jigar Mehta <jigarius@gmail.com>
2021-01-08 18:08:10 -0500

    Simplify query

--- lib/spca/pet_list.rb
+++ lib/spca/pet_list.rb
@@ -5,8 +5,7 @@ module SPCA
     # Create from Nokogiri::XML::Element for an SPCA list page.
     # E.g. HTML for a page containing a list of cats.
     def self.from_element(el)
-      el.at_css('#page-main .section--grid')
-        .search('.pet--card')
+      el.search('#page-main .section--grid .pet--card')
         .map do |item|
           PetCard.from_element(item)
         end
