beac872317d295b1b798e8b7da6038de4460f8af
Jigar Mehta <jigarius@gmail.com>
2021-01-08 16:48:27 -0500

    Require all classes in bootstrap

--- app/bootstrap.rb
+++ app/bootstrap.rb
@@ -2,8 +2,15 @@
 
 require 'pry'
 require 'json'
+require 'nokogiri'
+require 'net/http'
 
-require_relative '../lib/spca/scanner'
+require_relative '../lib/spca'
+require_relative '../lib/spca/cache'
+require_relative '../lib/spca/fetcher'
+require_relative '../lib/spca/pet_card'
+require_relative '../lib/spca/pet_list'
+require_relative '../lib/spca/image'
 
 # Configuration
 Pry.config.pager = false
