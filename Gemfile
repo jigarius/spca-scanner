e22dbfa675802b5e1b72f7ac800e8a62a29aafac
Jigar Mehta <jigarius@gmail.com>
2021-01-09 02:25:16 -0500

    Add and configure 'mail' and 'dotenv'

--- Gemfile
+++ Gemfile
@@ -7,9 +7,8 @@ ruby '2.7.2'
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem 'nokogiri', '~> 1.11', '>= 1.11.1'
-
-gem 'pry', '~> 0.13.1', group: :development
-
-gem 'rspec', '~> 3.10', group: [:development, :test]
-
 gem 'mail', '~> 2.7', '>= 2.7.1'
+
+gem 'dotenv', '~> 2.1', '>= 2.1.1', group: :development
+gem 'pry', '~> 0.13.1', group: :development
+gem 'rspec', '~> 3.10', group: [:development, :test]
--- app/bootstrap.rb
+++ app/bootstrap.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
+require 'dotenv'
 require 'pry'
 require 'digest'
+require 'mail'
 require 'nokogiri'
 require 'net/http'
 
@@ -11,7 +13,10 @@ require_relative '../lib/spca/fetcher'
 require_relative '../lib/spca/pet_card'
 require_relative '../lib/spca/pet_list'
 require_relative '../lib/spca/image'
+require_relative '../lib/spca/mail'
 require_relative '../lib/spca/scanner'
 
 # Configuration
 Pry.config.pager = false
+
+ENV.merge! Dotenv.load("#{SPCA::ROOT_PATH}/.env")
