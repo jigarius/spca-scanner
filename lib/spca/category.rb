276acfba39868b6beb69e51c97a59fbfc04382d2
Jigar Mehta <hello@jigarius.com>
2021-02-10 11:45:35 -0500

    Create SPCA::Category

---
+++ lib/spca/category.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+module SPCA
+  class Category
+    attr_reader :id, :uri
+
+    def initialize(id: , uri:)
+      @id = id
+      @uri = uri
+    end
+    private_methods(:initialize)
+
+    CATS = Category.new(
+      id: 'cats',
+      uri: URI.parse('https://www.spca.com/en/adoption/cats-for-adoption/')
+    )
+    DOGS = Category.new(
+      id: 'dogs',
+      uri: URI.parse('https://www.spca.com/en/adoption/dogs-for-adoption/')
+    )
+    RABBITS = Category.new(
+      id: 'rabbits',
+      uri: URI.parse('https://www.spca.com/en/adoption/rabbits-for-adoption/')
+    )
+    BIRDS = Category.new(
+      id: 'birds',
+      uri: URI.parse('https://www.spca.com/en/adoption/birds-for-adoption/')
+    )
+    OTHER = Category.new(
+      id: 'other',
+      uri: URI.parse('https://www.spca.com/en/adoption/small-animals-for-adoption/')
+    )
+
+    def self.all
+      {
+        CATS.id => CATS,
+        DOGS.id => DOGS,
+        RABBITS.id => RABBITS,
+        BIRDS.id => BIRDS,
+        OTHER.id => OTHER,
+      }
+    end
+
+    def self.from_id(id)
+      Category.all.each { |k, c| return c if k == id }
+
+      raise ArgumentError, "Unrecognized ID: #{id}"
+    end
+  end
+end
---
+++ spec/spca/category_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'spca/cache'
+
+module SPCA
+  describe Category do
+    it '.all' do
+      expectation = {
+        'cats' => Category::CATS,
+        'dogs' => Category::DOGS,
+        'rabbits' => Category::RABBITS,
+        'birds' => Category::BIRDS,
+        'other' => Category::OTHER,
+      }
+
+      expect(Category.all).to eq(expectation)
+    end
+
+    it '.from_id returns a category for valid ID' do
+      expect(Category.from_id('cats')).to be(Category::CATS)
+      expect(Category.from_id('dogs')).to be(Category::DOGS)
+    end
+
+    it '.from_id raises ArgumentError for invalid ID' do
+      expect { Category.from_id('dragon') }
+        .to raise_error(ArgumentError, 'Unrecognized ID: dragon')
+    end
+  end
+end
