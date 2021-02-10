712873c7f2835bb64d9fa05fcf6d01a1cd451cd2
Jigar Mehta <hello@jigarius.com>
2021-02-10 09:42:05 -0500

    Set SELECTOR as a constant

--- lib/spca/pet_list.rb
+++ lib/spca/pet_list.rb
@@ -2,10 +2,13 @@
 
 module SPCA
   class PetList
+    SELECTOR = '#page-main .section--grid .pet--card'
+    private_constant(:SELECTOR)
+
     # Create from Nokogiri::XML::Element for an SPCA list page.
     # E.g. HTML for a page containing a list of cats.
     def self.from_element(el)
-      el.search('#page-main .section--grid .pet--card')
+      el.search(SELECTOR)
         .map do |item|
           PetCard.from_element(item)
         end
