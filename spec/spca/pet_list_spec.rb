1be57c978afc8236349b1512f57c24d8b0a30443
Jigar Mehta <hello@jigarius.com>
2021-02-10 10:40:06 -0500

    Remove unnecessary constant: SAMPLE_FILE

--- spec/spca/pet_list_spec.rb
+++ spec/spca/pet_list_spec.rb
@@ -4,10 +4,10 @@ require 'spca/pet_list'
 
 module SPCA
   describe PetList do
-    SAMPLE_FILE = SPCA::ROOT_PATH + '/spec/fixtures/list.html'
-
     it '.from_element creates a PetList' do
-      el = Nokogiri::HTML.parse(File.open(SAMPLE_FILE))
+      el = Nokogiri::HTML.parse(
+        File.open(SPCA::ROOT_PATH + '/spec/fixtures/list.html')
+      )
       list = PetList.from_element(el)
 
       expect(list).to be_a_kind_of(Array)
