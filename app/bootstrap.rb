ed54f5513f3cccdcdaca0575933fee5718290221
Jigar Mehta <hello@jigarius.com>
2021-04-29 22:27:49 -0400

    Move SPCA requires to spca.rb

--- app/bootstrap.rb
+++ app/bootstrap.rb
@@ -7,15 +7,6 @@ require 'net/http'
 Bundler.require(:default, :development)
 
 require_relative '../lib/spca'
-require_relative '../lib/spca/cache'
-require_relative '../lib/spca/fetcher'
-require_relative '../lib/spca/category'
-require_relative '../lib/spca/pet_card'
-require_relative '../lib/spca/pet_list'
-require_relative '../lib/spca/image'
-require_relative '../lib/spca/mail'
-require_relative '../lib/spca/scanner'
-require_relative '../lib/spca/cli'
 
 # Configuration
 Pry.config.pager = false
--- lib/spca.rb
+++ lib/spca.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+require_relative 'spca/cache'
+require_relative 'spca/fetcher'
+require_relative 'spca/category'
+require_relative 'spca/pet_card'
+require_relative 'spca/pet_list'
+require_relative 'spca/image'
+require_relative 'spca/mail'
+require_relative 'spca/scanner'
+require_relative 'spca/cli'
+
 module SPCA
   ROOT_PATH = File.expand_path(File.dirname(File.dirname(__FILE__)))
 
